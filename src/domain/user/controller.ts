b295b743e060e908b9bedb19b7671d0a8d4cb573
POZA <poza@POZAs-MacBook-Pro.local>
2023-10-31 03:37:04 +0100

    create new user and edit user endpoints

--- src/domain/user/controller.ts
+++ src/domain/user/controller.ts
@@ -10,12 +10,60 @@ export const getUser = async () => {
 
 // Get single user by id
 export const getUserById = async (data: { id: number }) => {
-  // Get id of user
-  const { id } = data; 
-
+  const { id } = data; // Get id of user 
   // Find id in database
   const user = await prisma.user.findUnique({
     where: { id : Number(id) },
   });
   return user;
+}
+
+// Create user
+export const createUser = async (data: {
+  email: string;
+  name: string;
+  username: string;
+  bio?: string; 
+}) => {
+  const { email, name, username, bio} = data;
+  try {
+    // Create user in database
+    const result = await prisma.user.create({
+      data: {
+        email,
+        name,
+        username,
+        bio: 'Hey there',
+      },
+    });
+
+   return result;
+  } catch (e) {
+     throw e;
+  } 
+}
+
+// Update User 
+export const editUser = async (
+  userId: {
+    id: number 
+  },
+  data: {
+    bio: string,
+    name: string,
+    image: string
+  }
+) => {
+  const { id } = userId; // Get user id
+  const { bio, name, image } = data; // Get user data
+  try {
+    // Update user in database
+    const result = await prisma.user.update({
+      where: { id: Number(id) },
+      data: { bio, name, image },
+    });
+   return result;
+  } catch (e) {
+    throw e;
+  }
 }
--- src/domain/user/routes.ts
+++ src/domain/user/routes.ts
@@ -1,9 +1,10 @@
 // userRoutes.ts
 import { Router, Request, Response } from 'express';
-import { getUser, getUserById } from './controller';
+import { createUser, editUser, getUser, getUserById } from './controller';
 
 const userRouter: Router = Router();
 
+// Get all users
 userRouter.get('/', async (req: Request, res: Response) => {
   try {
     const users = await getUser();
@@ -13,6 +14,7 @@ userRouter.get('/', async (req: Request, res: Response) => {
   }
 });
 
+// Get a single user
 userRouter.get('/:id', async (req: Request, res: Response) => {
   try {
     const { id } = req.params;
@@ -20,6 +22,42 @@ userRouter.get('/:id', async (req: Request, res: Response) => {
     res.json(userId);
   } catch (error) {
     res.status(500).json({ error: 'An error occurred' });
+  }
+});
+
+// Create a new user
+userRouter.post('/', async (req: Request, res: Response) => {
+  try {
+    const { email, name, username, bio = 'Hey there' } = req.body;
+    const newUser = await createUser({
+      email, 
+      name,
+      username,
+      bio
+    });
+    res.json(newUser);
+  } catch (error) {
+    res.status(400).json({ error: 'Username and email should be unique' });
+  }
+});
+
+// Edit user
+userRouter.put('/:id', async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    const { bio, name, image } = req.body;
+
+    const editedUser = await editUser(
+      { id: Number(id) },
+      {
+        bio,
+        name,
+        image,
+    });
+
+    res.json(editedUser);
+  } catch (error) {
+    res.status(400).json({ error: 'Failed to update user' });
   }
 });
 
